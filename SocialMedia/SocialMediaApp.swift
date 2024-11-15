a540db5c17ad8a9aaed6c8adac9fa0e8b7773baf
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-11-15 15:52:01 +0400

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Hakob Ghlijyan on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
