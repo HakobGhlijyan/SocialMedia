4cb56f06565e241466523dcc3d63d762ff4b63df
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-12-06 13:21:10 +0400

    [Features] add log in view add register view add in register view photopicker

--- SocialMedia/View/LoginView.swift
+++ SocialMedia/View/LoginView.swift
@@ -6,13 +6,225 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct LoginView: View {
+    @State private var emailID: String = ""
+    @State private var password: String = ""
+    @State var createAccount: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 10.0) {
+            Text("Let's Sign you in")
+                .font(.largeTitle.bold())
+                .hAlign(.leading)
+            
+            Text("Welcome Back, \nYou have been missed")
+                .font(.title3)
+                .hAlign(.leading)
+            
+            VStack(spacing: 12.0) {
+                TextField("Email", text: $emailID)
+                    .textContentType(.emailAddress)
+                    .border(1, .gray.opacity(0.5))
+                    .padding(.top, 25)
+                
+                SecureField("Password", text: $password)
+                    .textContentType(.emailAddress)
+                    .border(1, .gray.opacity(0.5))
+                
+                Button("Reset Password ?") {
+                    //
+                }
+                .font(.callout)
+                .fontWeight(.medium)
+                .tint(.black)
+                .hAlign(.trailing)
+                
+                Button {
+                    
+                } label: {
+                    Text("Sign In")
+                        .foregroundStyle(.white)
+                        .hAlign(.center)
+                        .fillView(.black)
+                }
+                .padding(.top, 10)
+            }
+            
+            HStack {
+                Text("Don't have an account?")
+                    .foregroundStyle(.gray)
+                
+                Button("Register Now") {
+                    createAccount.toggle()
+                }
+                .bold()
+                .foregroundStyle(.black)
+            }
+            .font(.callout)
+            .vAlign(.bottom)
+        }
+        .vAlign(.top)
+        .padding(15)
+        .fullScreenCover(isPresented: $createAccount) {
+            RegisterView()
+        }
     }
 }
 
 #Preview {
     LoginView()
+}
+
+struct RegisterView: View {
+    @State private var emailID: String = ""
+    @State private var password: String = ""
+    @State private var userName: String = ""
+    @State private var userBio: String = ""
+    @State private var userBioLink: String = ""
+    @State private var userProfilePicData: Data?
+    
+    @State private var showImagePicker: Bool = false
+    @State private var photoItem: PhotosPickerItem?
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        VStack(spacing: 10.0) {
+            Text("Let's Register\nAccount")
+                .font(.largeTitle.bold())
+                .hAlign(.leading)
+            
+            Text("Hello user, have a wonderful journey")
+                .font(.title3)
+                .hAlign(.leading)
+            
+            ViewThatFits {
+                ScrollView(.vertical, showsIndicators: false) {
+                    HelperView()
+                }
+                HelperView()
+            }
+                        
+            HStack {
+                Text("Already have an account?")
+                    .foregroundStyle(.gray)
+                
+                Button("Login Now") {
+                    dismiss()
+                }
+                .bold()
+                .foregroundStyle(.black)
+            }
+            .font(.callout)
+            .vAlign(.bottom)
+        }
+        .vAlign(.top)
+        .padding(15)
+        .photosPicker(isPresented: $showImagePicker, selection: $photoItem)
+        .onChange(of: photoItem) { oldValue, newValue in
+            if let newValue {
+                Task {
+                    do {
+                        guard let imageData = try await newValue.loadTransferable(type: Data.self) else { return }
+                        await MainActor.run {
+                            userProfilePicData = imageData
+                        }
+                    } catch {
+                    //
+                    }
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder func HelperView() -> some View {
+        VStack(spacing: 12.0) {
+            ZStack {
+                if let userProfilePicData , let image = UIImage(data: userProfilePicData) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                } else {
+                    Image("NullProfile")
+                        .resizable()
+                        .scaledToFill()
+                        .background(.red)
+                }
+            }
+            .frame(width: 85, height: 85)
+            .clipShape(Circle())
+            .contentShape(Circle())
+            .onTapGesture {
+                showImagePicker.toggle()
+            }
+            .padding(.top, 25)
+            
+            TextField("User Name", text: $userName)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            TextField("Email", text: $emailID)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            SecureField("Password", text: $password)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            TextEditor(text: $userBio)
+                .frame(minHeight: 100, alignment: .top)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+                        
+            TextField("Bio Link", text: $userBioLink)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            Button {
+                
+            } label: {
+                Text("Sign up")
+                    .foregroundStyle(.white)
+                    .hAlign(.center)
+                    .fillView(.black)
+            }
+            .padding(.top, 10)
+        }
+    }
+}
+
+//MARK: - UI design Helper function
+extension View {
+    func hAlign(_ alignment: Alignment) -> some View {
+        self.frame(maxWidth: .infinity, alignment: alignment)
+    }
+    
+    func vAlign(_ alignment: Alignment) -> some View {
+        self.frame(maxHeight: .infinity, alignment: alignment)
+    }
+}
+
+//MARK: - UI design Custom Border
+extension View {
+    func border(_ width: CGFloat, _ color: Color) -> some View {
+        self
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+            .background {
+                RoundedRectangle(cornerRadius: 5, style: .continuous)
+                    .stroke(color, lineWidth: width)
+            }
+    }
+    
+    func fillView(_ color: Color) -> some View {
+        self
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+            .background {
+                RoundedRectangle(cornerRadius: 5, style: .continuous)
+                    .fill(color)
+            }
+    }
 }
