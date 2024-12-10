534ec6f663448b6917fb26265162cd3b1c7a208c
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-12-10 17:03:57 +0400

    [Features] end... but Module 03 is post error .. next fix

---
+++ SocialMedia/Model/Post.swift
@@ -0,0 +1,40 @@
+//
+//  Post.swift
+//  SocialMedia
+//
+//  Created by Hakob Ghlijyan on 12/10/24.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
+import FirebaseDatabase
+
+struct Post: Codable, Identifiable {
+    @DocumentID var id: String?
+    var text: String
+    var imageURL: URL?
+    var imageReferenceID: String = ""
+    var publishedDate: Date = Date()
+    var likedIDs: [String] = []
+    var dislikedIDs: [String] = []
+    // MARK: Basic User Info
+    var userName: String
+    var userUID: String
+    var userProfileURL: URL
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case text
+        case imageURL
+        case imageReferenceID
+        case publishedDate
+        case likedIDs
+        case dislikedIDs
+        case userName
+        case userUID
+        case userProfileURL
+    }
+}
---
+++ SocialMedia/View/MainView/PostsView.swift
@@ -0,0 +1,39 @@
+//
+//  PostsView.swift
+//  SocialMedia
+//
+//  Created by Hakob Ghlijyan on 12/10/24.
+//
+
+import SwiftUI
+
+struct PostsView: View {
+    @State private var createdNewPost: Bool = false
+    
+    var body: some View {
+        Text("Posts View")
+            .hAlign(.center).vAlign(.center)
+            .overlay(alignment: .bottomTrailing) {
+                Button {
+                    createdNewPost.toggle()
+                } label: {
+                    Image(systemName: "plus")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .padding(15)
+                        .background(.black, in: .circle)
+                }
+                .padding(15)
+            }
+            .fullScreenCover(isPresented: $createdNewPost) {
+                CreateNewPost { post in
+                    
+                }
+            }
+    }
+}
+
+#Preview {
+    PostsView()
+}
